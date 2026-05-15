80fb932cc240a5de9ef5b36a120cd06cc16ca67b
cwpacademysch-crypto <cwpacademysch@gmail.com>
2026-05-15 02:16:15 +0100

    test: Add environment debugging endpoint

--- app/main.py
+++ app/main.py
@@ -373,10 +373,3 @@ def agent_chat(
 
         raise HTTPException(status_code=500, detail=str(error))
     
-@app.get("/debug/env")
-def debug_env():
-    return {
-        "has_openai_key": bool(settings.OPENAI_API_KEY),
-        "openai_model": settings.OPENAI_MODEL,
-    }
-    
